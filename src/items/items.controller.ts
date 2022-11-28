3da1adc64140333c300afe9542703426df614748
Cloud-Xero <hiropon201511@gmail.com>
2022-11-28 20:58:01 +0900

    feat: import ItemsService & use constructor for DI

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ItemsService } from './items.service';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  // ItemsServiceクラスをインスタンス化し、変数に代入
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll() {
+    return this.itemsService.findAll();
+  }
+}
