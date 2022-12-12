536092198af7e95eae39c2e499c95f9653179b7f
mercari-komamiya <tmp-h-komamiya@mercari.com>
2022-12-12 21:53:13 +0900

    test: create sample test file

---
+++ src/items/example.spec.ts
@@ -0,0 +1,8 @@
+describe('jestの練習', () => {
+  it('test1', () => {
+    const result = 1;
+    const expected = 1;
+    expect(result).toBe(expected);
+    expect(result).toBe(expected);
+  });
+});
